1890d239b864b83082ca7329910bbe1c247aecf7
H-morii <haruki.mori21@gmail.com>
2024-02-18 21:53:01 +0900

    feat/Main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,7 +5,7 @@ import requests from '../Requests'
 const Main = () => {
   const [movies, setMovies] = useState([]);
   const movie = movies[Math.floor(Math.random() * movies.length)] 
-  const movieImg = `https://image.tmdb.org/t/p/original/${movie.backdrop_path}`
+  const movieImg = `https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`
   
   //  function getPopular() {
   //   axios.get(requests.requestPopular).then((response) => {
@@ -31,7 +31,6 @@ const Main = () => {
   try {
     const response = await axios.request(options)
     setMovies(response.data.results)
-    console.log(response.data.results)
   } catch (err) {
     console.log(err.message)
     }
@@ -40,12 +39,30 @@ const Main = () => {
   useEffect (() => {
     getPopular()
     },[])
+    // console.log(movie)
+
+    const trucateString = (str, num) => {
+      if(str?.length > num ) {
+        return str.slice(0, num) + '...'
+      } else {
+        return str
+      }
+    }
 
   return (
     <div className='w-full h-[550px] text-white'>
-      <div>
+      <div className='w-full h-full'>
         <div className="absolute w-full h-[550px] bg-gradient-to-r from-black"></div>
         <img className='w-full h-full object-cover' src={movieImg} alt={movie.title} />
+        <div className='absolute top-[20%] py-4 px-4' >
+          <h1 className=" text-3xl md:text-5xl mx-auto ">{movie?.title}</h1>
+          <p className='w-full md:max-w-[80%] lg:max-w-[70%] xl:max-w-[50%] text-gray-200' >{trucateString(movie.overview, 150)}</p>
+          <div className='my-4'>
+            <button className='border bg-gray-300 border-gray-300 text-black py-2 px-5' >Where to watch</button>
+            <button className='border border-gray-300 text-white py-2 px-5 ml-4'>Rate</button>
+         </div>
+         <p className="text-grey-400 text-small">{movie.release_date}</p>
+        </div>
       </div>
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <div className='flex items-center justify-between p-4 z-[100] w-full absolute'>
       <img src={Logo} className='cursor-pointer'></img>
       <div>
-        <button className='w-[173.57px] h-[38.30px] opacity-75 bg-stone-950 rounded-[4.88px] border border-stone-50 text-white transitionduration-300 hover:opacity-50 transition'>Sign in</button>
+        <button className='w-[173.57px] h-[38.30px] opacity-75 bg-stone-950 rounded-[4.88px] m-4 border border-stone-50 text-white transitionduration-300 hover:opacity-50 transition'>Sign in</button>
         <button className='bg-red-600 text-1xl font-semibold w-[115.88px] h-[40.30px] rounded-[4.88px] cursor-pointer transitionduration-300 hover:opacity-70 transition'>Login</button>
       </div>
     </div>
